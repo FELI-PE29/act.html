4bcd09a99275cae1fc71b620820f87dfb3fa92d2
FELIPE <afelipeplazas@ucundinamarca.edu.co>
2026-04-08 23:17:53 -0500

    Add styles for body and form elements

--- formulario3/script.js
+++ formulario3/script.js
@@ -1,1 +1,16 @@
+body {
+    font-family: Arial;
+    background: linear-gradient(to right, #74ebd5, #acb6e5);
+}
 
+form {
+    width: 400px;
+    margin: auto;
+    background: white;
+    padding: 20px;
+    border-radius: 10px;
+}
+
+input:focus {
+    border: 2px solid blue;
+}
